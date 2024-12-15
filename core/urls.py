6ab81e1ce38a5a4aa93ce44a88b3e68da444291d
Muhammed Zohaib Anwer <zohaibanwer2002@gmail.com>
2024-12-15 11:54:21 +0500

    added endpoints to update account details

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('dj_rest_auth.urls')),  # Login, Logout, Password Reset, etc.
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),  # Registration
-    path('api/', include('api.urls')),  # Your app's endpoints
+    path('api/', include('api.urls')),
+    path('api/', include('user.urls')), 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
